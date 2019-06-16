d97fdfecd9af5b0f445c489f64bf5119ad764281
drtran <DrKietTran@gmail.com>
2019-06-16 08:48:39 -0400

    Added FlightsByCareer code

---
+++ src/main/java/com/drkiettran/mapreduce/FlightsByCarrierMapper.java
@@ -0,0 +1,21 @@
+package com.drkiettran.mapreduce;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import au.com.bytecode.opencsv.CSVParser;
+
+public class FlightsByCarrierMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	@Override
+	protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+		if (key.get() > 0) {
+			String[] lines = new CSVParser().parseLine(value.toString());
+			context.write(new Text(lines[8]), new IntWritable(1)); // the 8th index is that for the name of airline
+																	// carrier
+		}
+	}
+}
